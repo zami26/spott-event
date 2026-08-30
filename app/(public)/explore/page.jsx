e34cbdef01b76d9f3a6c8f9892d25039ba3789b4
zami26 <arafatulislamzami@gmail.com>
2026-08-30 12:39:01 +0600

    explore page done. newly added the popular event across the country and no event part

--- app/(public)/explore/page.jsx
+++ app/(public)/explore/page.jsx
@@ -226,19 +226,45 @@ const ExplorePage = () => {
 
       {/* popular events across the country section */}
       {popularEvents && popularEvents.length > 0 && (
-  <div className="mb-16">
-    <div className="mb-6">
-      <h2 className="text-3xl font-bold mb-1">Popular Across Country</h2>
-      <p className="text-muted-foreground">Trending event nationwide</p>
-    </div>
-  </div>
-)}
+        <div className="mb-16">
+          <div className="mb-6">
+            <h2 className="text-3xl font-bold mb-1">Popular Across Country</h2>
+            <p className="text-muted-foreground">Trending event nationwide</p>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {popularEvents.map((event) => (
+              <EventCard
+                key={event._id}
+                event={event}
+                variant='list'
+                onClick={() => handleEventClick(event.slug)}
+              />
+            ))}
+
+          </div>
+        </div>
+      )}
 
 
-      {/* popular events across the country section */}
+
 
 
       {/* empty state */}
+      {
+        !loadingFeatured && !loadingLocal && !loadingPopular && (!featuredEvents || featuredEvents.length === 0) && (!localEvents || localEvents.length === 0) && (!popularEvents || popularEvents.length === 0) && (
+          <Card className="p-12 text-center">
+            <div className="max-w-md mx-auto space-y-4">
+              <div className="text-6xl mb-4"> </div>
+              <h2 className="text-2xl fond-bold">No events yet</h2>
+              <p className="text-muted-foreground">Be the first to create an event in your area</p>
+              <Button
+                 className="gap-2">
+                <a href='/create-event'>Create Event</a> </Button>
+            </div>
+          </Card>
+        )
+      }
 
 
 
